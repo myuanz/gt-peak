56cc555e03b9679502c0233931e2a134d900c984
myuanz <provefars@gmail.com>
2024-02-08 23:35:43 +0800

    一份基础的封装改进

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,85 @@
+use native_tls::TlsConnector;
+use native_tls::TlsStream;
 use socket2::{Domain, SockAddr, Socket, Type};
+use std::io::Read;
 use std::io::{self, Write};
 use std::net::{SocketAddr, TcpStream};
-use native_tls::TlsConnector;
-use std::io::Read;
+use std::time::{Duration, SystemTime};
 
-fn main() -> io::Result<()> {
-    // 创建一个socket
-    let socket = Socket::new(Domain::IPV4, Type::STREAM, None)?;
+#[derive(Debug)]
+struct SpeedTestResult {
+    start_time: SystemTime,
+    connect_time: SystemTime,
+    send_time: SystemTime,
+    recv_len: u64,
+    end_time: SystemTime,
+}
 
-    // 连接至 github.com 的 443 端口
-    let address: SocketAddr = "140.82.112.4:443".parse().unwrap();
-    let address = SockAddr::from(address);
-    
-    socket.connect(&address)?;
+// 定义一个新的trait来统一Read和Write操作
+pub trait ReadWrite: Read + Write {}
+impl ReadWrite for TcpStream {}
+impl ReadWrite for TlsStream<TcpStream> {}
 
-    // 转换socket2的Socket为std::net::TcpStream
-    let stream = TcpStream::from(socket);
+impl SpeedTestResult {
+    fn latency(&self) -> Duration {
+        self.connect_time.duration_since(self.start_time).unwrap()
+    }
+    /// 计算下载速度
+    ///
+    /// 单位：字节/秒
+    fn download_speed(&self) -> f64 {
+        let time = self.end_time.duration_since(self.send_time).unwrap();
+        self.recv_len as f64 / time.as_secs_f64()
+    }
+    fn from_http(ip: &str, port: u16, host: &str, path: &str) -> SpeedTestResult {
+        let start_time = SystemTime::now();
 
-    // 创建TlsConnector来处理SSL连接
-    let connector = TlsConnector::new().unwrap();
-    let mut stream = connector.connect("github.com", stream).unwrap();
+        let socket = Socket::new(Domain::IPV4, Type::STREAM, None).unwrap();
+        let address: SocketAddr = format!("{}:{}", ip, port).parse().unwrap();
+        let address = SockAddr::from(address);
+        socket.connect(&address).unwrap();
+        let connect_time = SystemTime::now();
 
-    // 发送HTTP请求
-    let request = "GET / HTTP/1.1\r\nHost: github.com\r\nConnection: close\r\n\r\n";
-    let _ = stream.write_all(request.as_bytes())?;
+        let stream = TcpStream::from(socket);
+        let mut stream: Box<dyn ReadWrite> = if port == 443 {
+            let connector = TlsConnector::new().unwrap();
+            let tls_stream = connector.connect(host, stream).unwrap();
+            Box::new(tls_stream)
+        } else {
+            Box::new(stream)
+        };
 
-    // 读取响应（这里只是一个简单示例）
-    let mut buffer = [0; 64];
-    let mut len = 0;
-    loop {
-        let bytes_read = stream.read(&mut buffer)?;
-        len += bytes_read;
-        if bytes_read == 0 {
-            break;
+        let request = format!(
+            "GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n",
+            path, host
+        );
+        stream.write_all(request.as_bytes()).unwrap();
+        let send_time = SystemTime::now();
+
+        let mut buffer = [0; 1024];
+        let mut recv_len: u64 = 0;
+        loop {
+            let bytes_read = stream.read(&mut buffer).unwrap();
+            recv_len += bytes_read as u64;
+            if bytes_read == 0 {
+                break;
+            }
+        }
+        let end_time = SystemTime::now();
+        SpeedTestResult {
+            start_time,
+            connect_time,
+            send_time,
+            recv_len,
+            end_time,
         }
     }
+}
 
-    println!("Response: {}", len);
+fn main() -> io::Result<()> {
+    let res = SpeedTestResult::from_http("140.82.112.4", 443, "github.com", "/");
+    println!("{:?}", res);
+
 
     Ok(())
 }
